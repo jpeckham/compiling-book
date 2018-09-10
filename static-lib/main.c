490d33edff4f077c3df3acd3a8d779fc223d72a9
James Peckham <jdpeckham@gmail.com>
2018-09-09 22:52:02 -0500

    static library example

---
+++ static-lib/main.c
@@ -0,0 +1,10 @@
+extern int someValue;
+extern int someValue2;
+#include <stdio.h>
+
+void main()
+{
+    printf("%d\n", someValue);
+    printf("%d\n", someValue2);
+    return;
+}
---
+++ static-lib/second.c
@@ -0,0 +1,1 @@
+int someValue2 = 5;
